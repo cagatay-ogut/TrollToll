d0c8d33411ed2ac36ec0ecb5c1f24dc0ff4956c4
Cagatay <ogutcagatay@gmail.com>
2025-12-30 17:01:59 +0300

    Initial Commit

---
+++ TrollToll/TrollTollApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrollTollApp.swift
+//  TrollToll
+//
+//  Created by Cagatay on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrollTollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
