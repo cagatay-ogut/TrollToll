50ea17a4078dd17e481b94b70cb76f6937012171
Cagatay <ogutcagatay@gmail.com>
2026-01-13 22:40:31 +0300

    Fade out and in middle card when new card is drawn

--- TrollToll/View/SpriteKit/CardNode.swift
+++ TrollToll/View/SpriteKit/CardNode.swift
@@ -8,34 +8,49 @@
 import SpriteKit
 
 class CardNode: SKSpriteNode {
+    private var cardNode: SKShapeNode
+    private var labelNode: SKLabelNode
+
     var cardNumber: Int {
         didSet {
-            children.compactMap { $0 as? SKLabelNode }.first?.text = "\(cardNumber)"
+            if oldValue != cardNumber {
+                updateCard()
+            }
         }
     }
 
     init(cardNumber: Int, position: CGPoint, size: CGSize) {
         self.cardNumber = cardNumber
-        super.init(texture: nil, color: UIColor.purple, size: size)
-        self.position = position
-
-        let borderNode = SKShapeNode(
+        self.labelNode = SKLabelNode(text: "\(cardNumber)")
+        self.cardNode = SKShapeNode(
             rect: CGRect(origin: .init(x: -size.width / 2, y: -size.height / 2), size: size)
         )
-        borderNode.strokeColor = .black
-        let labelNode = SKLabelNode(text: "\(cardNumber)")
+        super.init(texture: nil, color: UIColor.clear, size: size)
+        self.position = position
+
+        cardNode.strokeColor = .black
+        cardNode.fillColor = .purple
         labelNode.horizontalAlignmentMode = .left
         labelNode.verticalAlignmentMode = .top
         labelNode.position = .init(x: -size.width / 2, y: size.height / 2)
         labelNode.fontSize = 12
         labelNode.fontName! += "-Bold"
 
-        self.addChild(borderNode)
-        self.addChild(labelNode)
+        cardNode.addChild(labelNode)
+        self.addChild(cardNode)
     }
 
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func updateCard() {
+        let fadeOut = SKAction.fadeAlpha(to: 0, duration: 0.3)
+        let updateNumber = SKAction.run {
+            self.labelNode.text = "\(self.cardNumber)"
+        }
+        let fadeIn = SKAction.fadeAlpha(to: 1, duration: 0.3)
+        cardNode.run(SKAction.sequence([fadeOut, updateNumber, fadeIn]))
     }
 }
